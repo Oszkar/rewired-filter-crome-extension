fe29fd010b1bf0c8e11894efee7fa3b451ffc349
Oszkar <jozsa.oszkar@gmail.com>
2016-12-24 12:32:16 +0900

    Add forum title and active username removal

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,8 +6,9 @@ chrome.extension.sendMessage({}, function(response) {
 		// for testing
 		var toRemoveUserNames = ['moken', 'psishock', 'kecske']
 
-		// below, we will separately remove the posts from the content pages and the topics fromthe main page
-		// we handle this two cases differently as we can only rely on the DOM and the DOM is very different in the content page and main page
+		// below, we will separately remove the posts from the content pages and the topics fromthe main page and the usernames from the statistics page
+		// we handle these cases differently as we can only rely on the DOM and the DOM is very different in the content page and main page
+		// the three loops are not the most efficient, but there will be probably only a few items in the list and who cares about efficiency on the web in 2016 anyways
 		// TODO even handled separately, post removal might be done a bit simpler
 
 		// ----------------------------------------------------
@@ -52,6 +53,26 @@ chrome.extension.sendMessage({}, function(response) {
 			});
 		});
 
+		// -------------------------------------------------------------
+		// Region: removal of user names from the bottom statistics pane
+		// -------------------------------------------------------------
+
+		// go through every username to filter
+		toRemoveUserNames.forEach(function(name) {
+			// username class is on every username href on the main page
+			$(".username").each(function(index, element) {
+				// text is the actual username that this href points to
+				var text = $(element).html().toLowerCase();
+				if(text === name) {
+					if ($(element).parents('.forum-statistics-sub-body').length) {
+						// then we are in the statistics pane and have to remove this href but not any of its parents
+						console.log("Removing a user name from the bottom statistics panel");
+						element.remove();
+					}
+				}
+			});
+		});
+
 		// ---------------------------------------------------------------------------------------
 		// Region: removal of topics from recents and favorites where the user has the latest post
 		// ---------------------------------------------------------------------------------------
@@ -69,6 +90,7 @@ chrome.extension.sendMessage({}, function(response) {
 					// for favorites and recents, the title of the actual topic is in a bit different places
 					var recentTitle = granpa.children(".views-field.views-field-title").children(".topik")[0];
 					var favoriteTitle = granpa.children(".views-field").children()[0];
+					var forumTitle = granpa.children(".forum-details").children(".forum-name").children()[0];
 					
 					if (recentTitle) {
 						// this case, it was a recent topic the user posted in
@@ -76,10 +98,15 @@ chrome.extension.sendMessage({}, function(response) {
 					} else if (favoriteTitle) {
 						// this case, it is a topic in the 'favorites' lists
 						console.log("Removing topic \"" + $(favoriteTitle).text() + "\" from favorites because of filtered a user");
-					} else  {
+					} else if (forumTitle) {
+						// this case, it is a topic in the 'favorites' lists
+						console.log("Removing forum \"" + $(forumTitle).text() + "\" from the main board because of filtered a user");
+					} else {
 						// this case, it is a topic in the 'favorites' lists
 						console.log("Whoops removing something unknown. This is either a bug or you are filtering yourself.");
-						console.log("Removed element: " + granpa);
+						console.log("Removed element:");
+						console.log(granpa);
+						console.log(element);
 					}
 					granpa.remove();
 				}
